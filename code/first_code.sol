1ec0d578beb8b71e5a56df57c01ecc9f806b8014
Monitor Laboratório 3 <monitorlab3@unifacef.local>
2021-10-20 16:53:46 -0300

    solidtiy notes

---
+++ code/first_code.sol
@@ -0,0 +1,7 @@
+/**
+To use this code we recommend to use the website: http://remix.ethereum.org/
+
+Select the Solidity Plugin, but not for now, go to the files and create inside the contracts folder a new file called "SimpleStorage.sol", this will be  alittle application to store information on the blockchain
+
+The simbol of solidity at your rigth side will be where you compile the code to the machine language, and it'll be diferents params to chose, don't worry about ;   
+ */
